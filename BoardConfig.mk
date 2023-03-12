81d5861aab6b5b98a82968b88aa20337ac7b757c
Gofaraway71 <88510377+Gofaraway71@users.noreply.github.com>
2023-03-12 09:06:44 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,9 @@ BOARD_AVB_RECOVERY_ADD_HASH_FOOTER_ARGS += \
 TARGET_BOOTLOADER_BOARD_NAME := mt6785
 TARGET_NO_BOOTLOADER := true
 
+# Workaround for error copying vendor files to recovery ramdisk
+TARGET_COPY_OUT_VENDOR := vendor
+
 # File systems
 
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 102760448
